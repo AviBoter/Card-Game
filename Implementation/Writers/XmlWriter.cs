542378eabce34ca3dfb8aa65545c6c122024d919
AviBoter <54690196+AviBoter@users.noreply.github.com>
2021-04-29 03:00:50 +0300

    Update XmlWriter.cs

--- Implementation/Writers/XmlWriter.cs
+++ Implementation/Writers/XmlWriter.cs
@@ -6,7 +6,9 @@ using Database.API.Card.Extra;
 using UnityEditor;
 using System.Runtime.Serialization;
 
-[DataContract]
+//due to CardData fields (getters/seeters) being private and i was denied modifying/changing the API and coudn't use data contract
+//nor transfer it by byte etc. i implemented manual xml writer.
+
 public class XmlWriter : IFileWriter
 {
     public void WriteToFile(string path, CardData[] Cards)
